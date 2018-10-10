1acd1f3722f27e5c28b9380da20bdb594736b086
Jeo D <e8gea6w5z8id@opayq.com>
2018-10-10 08:43:28 -0600

    Revert "Commit to prevent overwrite"
    This reverts commit c0c9efeeccb128747e14d655bb3f4627958cb5b5.

--- main.js
+++ main.js
@@ -5,82 +5,25 @@ var winningNumber = generateRandom();
 var clearButton = document.querySelector('.clear-button');
 var resetButton = document.querySelector('.reset-button');
 var submitButton = document.querySelector('.submit-button');
+clearButton.disabled = true;
+resetButton.disabled = true;
+
+function generateRandom() {
+  var randomNumber = Math.floor(Math.random() * (rangeDifference + 1)) + rangeBegin;
+  document.querySelector('.winning-number').innerText = randomNumber;
+  return randomNumber;
+}
 
 var updateClick = document.querySelector('.update-button');
 var submitClick = document.querySelector('.submit-button');
 var clearClick = document.querySelector('.clear-button');
 var resetClick = document.querySelector('.reset-button');
 var guessedNum = 0;
-var numGuessTries = 0;
-
-initializeForm();
 
 updateClick.addEventListener("click", function( ) {
   rangeBegin = parseInt(document.querySelector('#min-range').value);
   rangeEnd = parseInt(document.querySelector('#max-range').value);
   rangeDifference = rangeEnd - rangeBegin;
-  rangeValidation(rangeDifference);
-  resetButton.disabled = false;
-});
-
-submitClick.addEventListener("click", function( ) {
-  guessedNum = document.querySelector('#guess').value;
-  validateInteger(guessedNum);
-  guessCounter();
-});
-
-clearClick.addEventListener("click", function ( ) {
-  clearInput();
-});
-
-resetClick.addEventListener("click", function ( ) {
-  clearInput();
-  initializeForm();
-  winningNumber = generateRandom();
-});
-
-function initializeForm() {
-  clearInput();
-  rangeBegin = 1;
-  rangeEnd = 100;
-  numGuessTries = 0;
-  rangeDifference = rangeEnd - rangeBegin;
-  clearButton.disabled = true;
-  resetButton.disabled = true;
-  document.querySelector('.range-begin').innerText = 1;
-  document.querySelector('.range-end').innerText = 100;
-  document.querySelector('.last-guess').innerText = "";
-  document.querySelector('h2').innerText = "";
-  document.querySelector('.right-side h1').innerText = "Make a guess";
-  document.querySelector('#min-range').value = null;
-  document.querySelector('#max-range').value = null;
-}
-
-function generateRandom() {
-  var randomNumber = Math.floor(Math.random() * (rangeDifference + 1)) + rangeBegin;
-  // document.querySelector('.winning-number').innerText = randomNumber;
-  return randomNumber;
-}
-function guessCounter() {
-  numGuessTries += 1;
-  document.querySelector('h4').innerText = "Number of wrong guesses: " + numGuessTries;
-}
-
-function validateInteger(userNum) {
-  if (isNaN(userNum)) {
-    alert('Please enter integers only');
-  } else if (!(rangeBegin <= userNum && userNum <= rangeEnd)) {
-    alert('Please enter integers within the range');
-  } else {
-    document.querySelector('.last-guess').innerText = userNum;
-    checkGuess(userNum);
-    document.querySelector('.right-side h1').innerText = "You Guessed:"
-    clearButton.disabled = false;
-    resetButton.disabled = false;
-  }
-}
-
-function rangeValidation() {
   if (isNaN(rangeDifference)) {
     alert('Please enter integers only');
   } else if (rangeBegin > rangeEnd) {
@@ -90,10 +33,45 @@ function rangeValidation() {
     document.querySelector('.range-end').innerText = document.querySelector('#max-range').value;
     winningNumber = generateRandom();
   }
-}
+});
+
+submitClick.addEventListener("click", function( ) {
+  guessedNum = document.querySelector('#guess').value;
+  if (isNaN(guessedNum)) {
+    alert('Please enter integers only');
+  } else if (!(rangeBegin <= guessedNum && guessedNum <= rangeEnd)) {
+    alert('Please enter integers within the range');
+  } else {
+  document.querySelector('.last-guess').innerText = guessedNum;
+  checkGuess(guessedNum);
+  document.querySelector('.right-side h1').innerText = "You Guessed:"
+  clearButton.disabled = false;
+  resetButton.disabled = false;
+  }
+});
+
+clearClick.addEventListener("click", function ( ) {
+  clearInput();
+});
+
+resetClick.addEventListener("click", function ( ) {
+  clearInput();
+  rangeBegin = 1;
+  rangeEnd = 100;
+  rangeDifference = rangeEnd - rangeBegin;
+  document.querySelector('.range-begin').innerText = 1;
+  document.querySelector('.range-end').innerText = 100;
+  document.querySelector('.last-guess').innerText = "";
+  winningNumber = generateRandom();
+  document.querySelector('h2').innerText = "";
+  document.querySelector('.right-side h1').innerText = "Make a guess";
+  resetButton.disabled = true;
+});
 
 function clearInput() {
-  document.querySelector('#guess').value = null;
+  document.querySelector('#min-range').value = "";
+  document.querySelector('#max-range').value = "";
+  document.querySelector('#guess').value = "";
   clearButton.disabled = true;
 };
 
