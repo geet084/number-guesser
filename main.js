3e33ed2c02abad6c9caac7b451859c732dd9012e
Jeo D <e8gea6w5z8id@opayq.com>
2018-10-09 18:13:09 -0600

    Complete majority of functionality

--- main.js
+++ main.js
@@ -2,6 +2,11 @@ var rangeBegin = 1;
 var rangeEnd = 100;
 var rangeDifference = rangeEnd - rangeBegin;
 var winningNumber = generateRandom();
+var clearButton = document.querySelector('.clear-button');
+var resetButton = document.querySelector('.reset-button');
+var submitButton = document.querySelector('.submit-button');
+clearButton.disabled = true;
+resetButton.disabled = true;
 
 function generateRandom() {
   var randomNumber = Math.floor(Math.random() * (rangeDifference + 1)) + rangeBegin;
@@ -16,19 +21,33 @@ var resetClick = document.querySelector('.reset-button');
 var guessedNum = 0;
 
 updateClick.addEventListener("click", function( ) {
-  document.querySelector('.range-begin').innerText = document.querySelector('#min-range').value;
-  document.querySelector('.range-end').innerText = document.querySelector('#max-range').value;
   rangeBegin = parseInt(document.querySelector('#min-range').value);
   rangeEnd = parseInt(document.querySelector('#max-range').value);
   rangeDifference = rangeEnd - rangeBegin;
-  winningNumber = generateRandom();
+  if (isNaN(rangeDifference)) {
+    alert('Please enter integers only');
+  } else if (rangeBegin > rangeEnd) {
+    alert('Please ensure Min Range is less than Max Range')
+  } else {
+    document.querySelector('.range-begin').innerText = document.querySelector('#min-range').value;
+    document.querySelector('.range-end').innerText = document.querySelector('#max-range').value;
+    winningNumber = generateRandom();
+  }
 });
 
 submitClick.addEventListener("click", function( ) {
   guessedNum = document.querySelector('#guess').value;
+  if (isNaN(guessedNum)) {
+    alert('Please enter integers only');
+  } else if (!(rangeBegin <= guessedNum && guessedNum <= rangeEnd)) {
+    alert('Please enter integers within the range');
+  } else {
   document.querySelector('.last-guess').innerText = guessedNum;
   checkGuess(guessedNum);
   document.querySelector('.right-side h1').innerText = "You Guessed:"
+  clearButton.disabled = false;
+  resetButton.disabled = false;
+  }
 });
 
 clearClick.addEventListener("click", function ( ) {
@@ -46,13 +65,14 @@ resetClick.addEventListener("click", function ( ) {
   winningNumber = generateRandom();
   document.querySelector('h2').innerText = "";
   document.querySelector('.right-side h1').innerText = "Make a guess";
-
+  resetButton.disabled = true;
 });
 
 function clearInput() {
   document.querySelector('#min-range').value = "";
   document.querySelector('#max-range').value = "";
   document.querySelector('#guess').value = "";
+  clearButton.disabled = true;
 };
 
 function checkGuess(userGuess) {
